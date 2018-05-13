691bf6db4055cd355ac06ae30e20e5da2705e4a5
Alex Gochi <alex.gochi.javan@gmail.com>
2018-05-13 14:12:38 +0700

    Update

--- app/src/main/java/alexgochi/superb/helper/SessionManager.java
+++ app/src/main/java/alexgochi/superb/helper/SessionManager.java
@@ -1,5 +1,6 @@
 package alexgochi.superb.helper;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
@@ -10,22 +11,19 @@ public class SessionManager {
     private static String TAG = SessionManager.class.getSimpleName();
 
     // Shared Preferences
-    SharedPreferences pref;
+    private SharedPreferences pref;
 
-    Editor editor;
-    Context _context;
-
-    // Shared pref mode
-    int PRIVATE_MODE = 0;
+    private Editor editor;
 
     // Shared preferences file name
-    private static final String PREF_NAME = "AndroidHiveLogin";
+    private static final String PREF_NAME = "QRAbsentLogin";
 
     private static final String KEY_IS_LOGGED_IN = "isLoggedIn";
 
+    @SuppressLint("CommitPrefEdits")
     public SessionManager(Context context) {
-        this._context = context;
-        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        int PRIVATE_MODE = 0;
+        pref = context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
         editor = pref.edit();
     }
 
